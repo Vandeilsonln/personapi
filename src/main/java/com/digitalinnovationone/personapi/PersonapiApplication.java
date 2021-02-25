d9a433c8cd13ac1a7b6775ff97b2fd784e298064
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-02-25 00:28:30 -0300

    Initial commit

---
+++ src/main/java/com/digitalinnovationone/personapi/PersonapiApplication.java
@@ -0,0 +1,14 @@
+package com.digitalinnovationone.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+		String a ="Alo";
+	}
+
+}
