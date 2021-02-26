ead02fd513657291eaacab4b45adaf0372025432
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-02-26 00:40:49 -0300

    Improve person creation functionality. Upgrade mapstruct version to 1.4.1

--- src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
@@ -1,29 +1,32 @@
 package com.digitalinnovationone.personapi.controller;
 
 
+import com.digitalinnovationone.personapi.dto.request.PersonDTO;
 import com.digitalinnovationone.personapi.dto.response.MessageResponseDTO;
 import com.digitalinnovationone.personapi.entity.Person;
 import com.digitalinnovationone.personapi.repository.PersonRepository;
+import com.digitalinnovationone.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    private PersonRepository personRepository;
+    private PersonService personService;
 
     @Autowired
-    public PersonController(PersonRepository personRepository){
-        this.personRepository = personRepository;
+    public PersonController(PersonService personService){
+        this.personService = personService;
     }
 
     @PostMapping
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with Id " + savedPerson.getId())
-                .build();
-    };
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson (@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
+    }
+
 }
